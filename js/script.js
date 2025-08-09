6fb8fd35ebf24b936741fc1daf028e75c3fef22f
cristiano-opz <andreimodi21@gmail.com>
2025-08-09 19:54:53 -0300

    front end is in the process to the end
    I start yesterday to do the site's front. It has some bugs, but i'll fix.

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let content = document.getElementById('content');
+
+function clickMenu() {
+    if (content.style.display == 'block') {
+        content.style.display = 'none';
+    } else {
+        content.style.display = 'block';
+    }
+}
